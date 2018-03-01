cfa04d47df52a528373594b8c7b7d6e7e744d571
root <ajtaggart@dons.usfca.edu>
2018-03-01 13:55:14 -0800

    Commented out screen files

--- stage2C/02-net-tweaks/01-run.sh
+++ stage2C/02-net-tweaks/01-run.sh
@@ -6,3 +6,4 @@ install -v -m 644 files/wait.conf			${ROOTFS_DIR}/etc/systemd/system/dhcpcd.serv
 install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
 install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
 
+
