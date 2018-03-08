babe4663243996ef8b9394cca4ddf1f9be2972c1
root <ajtaggart@dons.usfca.edu>
2018-03-07 20:50:21 -0800

    Moved to predicatable interface names changes INS..sh accordingly

--- INSTRUCTIONS.sh
+++ INSTRUCTIONS.sh
@@ -3,19 +3,21 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y hostapd
 sudo apt-get install -y dnsmasq
 
-sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlx28f366aa5a6f.conf
 
 sudo echo "network={
         ssid=\"CSLabs\"
         psk=\"1kudlick\"
-}" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+}" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant-wlx28f366aa5a6f.conf
 
 sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
 
-# IF BUILT IN IS ON WLAN0 THEN SWAP WLAN1 FOR WLAN0
+# IF BUILT IN IS ON WLAN0 THEN SWAP WLAN1 FOR WLAN0 
+# builtin MUST be the access point as dongle needs AP Mode
+# ENDING MAC FOR DONGLE = ...:6f
 
-sudo echo "interface wlan1
+sudo echo "interface wlan0
 static ip_address=192.168.220.1/24
 static routers=192.168.220.0" | sudo tee -a /etc/dhcpcd.conf
 
@@ -23,7 +25,7 @@ sudo systemctl daemon-reload
 
 sudo service dhcpcd restart
 
-sudo echo "interface=wlan1
+sudo echo "interface=wlan0
 driver=nl80211
 ssid=Pi-AP
 channel=1
@@ -43,7 +45,7 @@ sudo sed -i '19s/.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/' /etc/init.d/hosta
 
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 
-sudo echo "interface=wlan1  
+sudo echo "interface=wlan0  
 listen-address=192.168.220.1
 server=8.8.8.8       # Use Google DNS  
 domain-needed        # Don't forward short names  
@@ -55,9 +57,9 @@ sudo iptables -X
 sudo iptables -F
 sudo iptables -t nat -X
 sudo iptables -t nat -F
-sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-sudo iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o wlx28f366aa5a6f -j MASQUERADE
+sudo iptables -A FORWARD -i wlx28f366aa5a6f -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o wlx28f366aa5a6f -j ACCEPT
 # -----------------------------------------------------------------
 # sudo iptables -X
 # sudo iptables -F
@@ -75,6 +77,8 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
 sudo sed -i '20i\iptables-restore < /etc/iptables.ipv4.nat\' /etc/rc.local
+
+sudo systemctl daemon-reload
 
 sudo systemctl start hostapd
 sudo systemctl start dnsmasq
