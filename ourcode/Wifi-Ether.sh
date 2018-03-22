fccac3d19653021743cd246e898c87b21a08917c
AlecTaggart <taggartalec@gmail.com>
2018-03-22 10:59:09 -0700

    minor organization clean up

---
+++ ourcode/Wifi-Ether.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# Share Wifi with Eth device
+# 
+# 
+# DO THIS STUFF BEFORE INSTALLING SCRIPT
+# sudo apt-get update -y && sudo apt-get upgrade -y
+# # sudo apt-get install -y dnsmasq
+
+ip_address="192.168.2.1"
+netmask="255.255.255.0"
+dhcp_range_start="192.168.2.2"
+dhcp_range_end="192.168.2.100"
+dhcp_time="12h"
+eth="eth0"
+wlan="wlan0"
+
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t nat -A POSTROUTING -o $wlan -j MASQUERADE  
+sudo iptables -A FORWARD -i $wlan -o $eth -m state --state RELATED,ESTABLISHED -j ACCEPT  
+sudo iptables -A FORWARD -i $eth -o $wlan -j ACCEPT 
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+sudo ifconfig $eth $ip_address netmask $netmask
+
+# Remove default route created by dhcpcd
+sudo ip route del 0/0 dev $eth &> /dev/null
+
+sudo systemctl stop dnsmasq
+
+echo -e "interface=$eth\n\
+bind-interfaces\n\
+server=8.8.8.8\n\
+domain-needed\n\
+bogus-priv\n\
+dhcp-range=$dhcp_range_start,$dhcp_range_end,$dhcp_time" > /etc/dnsmasq.conf
+
+# This file will work if resolveconf package is installed
+# Due to new updates in dnsmasq
+cp /etc/dnsmasq.conf /run/dnsmasq/resolv.conf
+
+sudo systemctl start dnsmasq
+
+# sudo sed -i '20i\sudo bash /run.sh\' /etc/rc.local
+
---
+++ ourcode/Wifi-Wifi.sh
@@ -0,0 +1,143 @@
+
+echo "
+Updating machine . . .
+"
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo "
+Installing host services . . .
+"
+sudo apt-get install -y dnsmasq
+sudo apt-get install -y hostapd
+sudo apt-get install -y rng-tools
+
+
+echo "
+Generating new wpa_supplicant . . .
+"
+
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+
+sudo echo "network={
+ssid=\"CSLabs\"
+psk=\"1kudlick\"
+}" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+
+echo "
+Stopping host serices . . .
+"
+
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
+
+
+echo "
+Updating dhcpcd.conf . . .
+"
+
+sudo echo "interface wlan0
+metric 150
+static ip_address=192.168.1.254/24
+#static routers=192.168.1.1
+#static domain_name_servers=192.168.1.1
+
+interface wlan1
+metric 100" | sudo tee -a /etc/dhcpcd.conf
+
+echo "
+Rebooting daemon and dhcpcd service . . .
+"
+
+sudo systemctl daemon-reload
+
+sudo service dhcpcd restart
+
+echo "
+Generating new hostapd.conf . . .
+"
+
+sudo echo "interface=wlan0
+driver=nl80211
+ssid=rpicluster-AP
+channel=1
+wmm_enabled=0
+wpa=1
+wpa_passphrase=rpicluster
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+auth_algs=1
+macaddr_acl=0
+logger_stdout=-1
+logger_stdout_level=2
+" | sudo tee /etc/hostapd/hostapd.conf
+
+echo "
+Linking new hostapd.conf . . .
+"
+
+sudo sed -i '10s/.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+
+sudo sed -i '19s/.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/' /etc/init.d/hostapd
+
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+
+echo "
+Generating new dnsmasq.conf . . .
+"
+
+sudo echo "no-resolv
+interface=wlan0
+listen-address=192.168.1.254
+server=8.8.8.8 # Use Google DNS
+domain-needed # Don't forward short names
+bogus-priv # Drop the non-routed address spaces.
+dhcp-range=192.168.1.100,192.168.1.150,12h # IP range and lease time
+#log each DNS query as it passes through
+log-queries
+dhcp-authoritative
+" | sudo tee /etc/dnsmasq.conf
+
+
+echo "
+Generating new iptable Rules . . .
+"
+
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE 
+sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+
+echo "
+Allowing ip_forward . . .
+"
+
+sudo sed -i '28 s/#//' /etc/sysctl.conf
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+echo "
+Saving / Restoring iptables . . .
+"
+
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+sudo sed -i '20i\iptables-restore < /etc/iptables.ipv4.nat\' /etc/rc.local
+
+echo "
+Starting host services . . .
+"
+
+sudo systemctl daemon-reload
+
+sudo systemctl start dnsmasq
+sudo systemctl start hostapd
+
+echo "
+Getting Tired . . Time to reboot . . .
+"
+sleep 1
+
+sudo reboot -h now
