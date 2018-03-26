4125a373925690bdbad876ce9817be2898569275
Derek Dang <dqdang17@gmail.com>
2018-03-26 15:58:01 -0700

    changes to network-manager

--- stage2S/02-net-tweaks/files/network-manager.py
+++ stage2S/02-net-tweaks/files/network-manager.py
@@ -11,21 +11,23 @@ def switch_disable(network):
 
 def switch_enable(option):
     switcher = {
-        1: "/rpicluster/network-manager/disable-wifi.sh",
-        2: "/rpicluster/network-manager/disable-switch.sh",
-        3: "/rpicluster/network-manager/disable-otg.sh",
+        1: "/rpicluster/network-manager/enable-wifi.sh",
+        2: "/rpicluster/network-manager/enable-switch.sh",
+        3: "/rpicluster/network-manager/enable-otg.sh",
     }
     return switcher.get(option, "Invalid Network Option")
-
 
 f = open("/rpicluster/network-manager/configured","r")
 network = f.read(1)
 print("Select a networking option:\n")
-option = input("1 = wifi to wifi\n2 = wifi to ethernet-switch\n3 = wifi to OTG")
+option = int(input("1 = wifi to wifi\n2 = wifi to ethernet-switch\n3 = wifi to OTG\n"))
 if(option != network):
-	os.system("sudo bash " + switch_disable(network))
+    print("Network: " + network)
+    print("Option: " + str(option))
+	os.system("sudo bash " + switch_disable(int(network)))
 	os.system("sudo bash " + switch_enable(option))
-	os.system("sudo echo " + option + " > /rpicluster/network-manager/configured")
+    os.system("sudo sh -c \"echo " + str(option) + " > /rpicluster/network-manager/configured\"")
+	# os.system("sudo echo " + str(option) + " > /rpicluster/network-manager/configured")
 
 else:
 	print("This network configuration is already active.")
