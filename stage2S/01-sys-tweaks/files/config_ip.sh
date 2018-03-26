8f92c99ea904fc45c27ae60f3f07b980a62cabdf
Derek Dang <dqdang17@gmail.com>
2018-03-26 14:17:53 -0700

    made ip_configuration and ssh-key updates

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -1,9 +1,8 @@
 sudo python config.py &
-P1=$!
 output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 for i in ${output[@]}
 do
     fab config_ip -u pi -H "$i"
 done
-wait $P1
+#pkill config.py
 
--- stage2S/01-sys-tweaks/files/send_SSH_keys.sh
+++ stage2S/01-sys-tweaks/files/send_SSH_keys.sh
@@ -1,6 +1,6 @@
+key=`ssh-keygen -t rsa -C pi@192.168.1.254`
 output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 for i in ${output[@]}
 do
-    key=`ssh-keygen -t rsa -C pi@192.168.0.254`
     fab send_SSH_keys "$key" -u pi -H "$i"
 done
