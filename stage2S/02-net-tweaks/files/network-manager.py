012257a7c8ad85d7080e46170c57a9a23b94dd50
AlecTaggart <taggartalec@gmail.com>
2018-03-26 22:13:55 -0700

    update

--- stage2S/02-net-tweaks/files/network-manager.py
+++ stage2S/02-net-tweaks/files/network-manager.py
@@ -18,18 +18,19 @@ def switch_enable(option):
     return switcher.get(option, "Invalid Network Option")
 
 f = open("/rpicluster/network-manager/configured","r")
-network = f.read(1)
+network = int(f.read(1))
 print("Select a networking option:\n")
-option = int(input("1 = wifi to wifi\n2 = wifi to ethernet-switch\n3 = wifi to OTG\n"))
+print("1 = wifi to wifi\n2 = wifi to ethernet-switch\n3 = wifi to OTG\n")
+option = int(input("Select a networking option: "))
 f.close()
 if(option != network):
-    print("Network: " + network)
+    print("Network: " + str(network))
     print("Option: " + str(option))
-    if(network != "0"):
-	    os.system("sudo bash " + switch_disable(int(network)))
+    if(network != 0):
+	    os.system("sudo bash " + switch_disable(network))
 	os.system("sudo bash " + switch_enable(option))
     os.system("sudo echo " + str(option) + " > /rpicluster/network-manager/configured")
-    print("Rebooting machine. . . ")
+    print("Rebooting machine . . . ")
     os.system("sudo reboot -h now")
 
 else:
