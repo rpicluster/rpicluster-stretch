81edd743dee381dc8694655d4644cb0720178ae5
AlecTaggart <taggartalec@gmail.com>
2018-03-27 16:37:05 -0700

    set-wifi changes

--- stage2S/02-net-tweaks/files/set-wifi.py
+++ stage2S/02-net-tweaks/files/set-wifi.py
@@ -1,7 +1,9 @@
 import sys
 
 location = str(sys.argv[1])
-print("- - - Set Up Wifi Interface - - -")
-ssid = str(input("\nEnter network ssid: "))
-psk = str(input("\nEnter network psk: "))
-os.system("sudo echo network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n} >> /etc/wpa_supplicant/" + location)
+print("- - - Set Up Wifi Interface - - -\n")
+ssid = input("Enter network ssid: ")
+print("\n")
+psk = input("Enter network psk: ")
+command = str("sudo echo network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n} >> /etc/wpa_supplicant/" + location)
+os.system(command)
