b1af9ff36a87ddc1391d2493b4e232e911e03c71
Derek Dang <dqdang17@gmail.com>
2018-03-27 20:53:10 -0700

    added things to TO-DO and reboot remotely

--- stage2S/01-sys-tweaks/files/fabfile.py
+++ stage2S/01-sys-tweaks/files/fabfile.py
@@ -3,6 +3,7 @@ from fabric.api import *
 def config_ip():
 	with cd('/rpicluster/config'):
 		run('sudo python calibrate_touchless.py')
+    run('sudo reboot -h now')
 
 def send_SSH_keys(ssh_key):
 	run('mkdir ~/.ssh/')
