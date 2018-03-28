37239cbc082806234a6db3b40253928f4b766e62
Derek Dang <dqdang17@gmail.com>
2018-03-27 20:37:22 -0700

    removed new line in set-wifi

--- stage2S/02-net-tweaks/files/set-wifi.py
+++ stage2S/02-net-tweaks/files/set-wifi.py
@@ -1,9 +1,8 @@
-import sys
+import os, sys
 
 location = str(sys.argv[1])
 print("- - - Set Up Wifi Interface - - -\n")
 ssid = raw_input("Enter network ssid: ")
-print("\n")
 psk = raw_input("Enter network psk: ")
 command = str("sudo echo \"network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n}\" >> /etc/wpa_supplicant/" + location)
 os.system(command)
