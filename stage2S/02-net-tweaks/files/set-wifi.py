bfdf8862216eb94205b30ca952a4a89d37efe39e
Derek Dang <dqdang17@gmail.com>
2018-03-27 21:39:06 -0700

    Debugging path object in set-wifi

--- stage2S/02-net-tweaks/files/set-wifi.py
+++ stage2S/02-net-tweaks/files/set-wifi.py
@@ -4,5 +4,8 @@ location = str(sys.argv[1])
 print("- - - Set Up Wifi Interface - - -\n")
 ssid = raw_input("Enter network ssid: ")
 psk = raw_input("Enter network psk: ")
-command = "\'sudo echo \"network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n}\" >> /etc/wpa_supplicant/" + location + "\'"
-os.system(command)
+# command = "\'sudo echo \"network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n}\" >> /etc/wpa_supplicant/" + location + "\'"
+echo_command = "network={\n    ssid=\"" + ssid + "\"\n    psk=\"" + psk + "\"\n}"
+full_command = "sudo echo " + echo_command + " >> /etc/wpa_supplicant/" + location
+print(full_command)
+os.system(full_command)
