7204ccaff921f1b3d534dc311c848709a21cc3dc
AlecTaggart <taggartalec@gmail.com>
2018-03-28 21:48:02 -0700

    fixed tabs in fab file

--- stage2S/01-sys-tweaks/files/fabfile.py
+++ stage2S/01-sys-tweaks/files/fabfile.py
@@ -1,11 +1,11 @@
 from fabric.api import *
 
 def config_ip():
-	with cd('/rpicluster/config'):
-		run('sudo python calibrate_touchless.py')
+    with cd('/rpicluster/config'):
+        run('sudo python calibrate_touchless.py')
     run('sudo reboot -h now')
 
 def send_SSH_keys(ssh_key):
-	run('mkdir ~/.ssh/')
-	run('touch ~/.ssh/authorized_keys')
-	run('echo {} >> ~/.ssh/authorized_keys'.format(ssh_key))
+    run('mkdir ~/.ssh/')
+    run('touch ~/.ssh/authorized_keys')
+    run('echo {} >> ~/.ssh/authorized_keys'.format(ssh_key))
