759d6c1e1c2b055f227e29b1dbfdde1c1d106871
AlecTaggart <taggartalec@gmail.com>
2018-03-28 20:55:40 -0700

    fixed client host naming

--- stage2C/02-net-tweaks/01-run.sh
+++ stage2C/02-net-tweaks/01-run.sh
@@ -16,4 +16,6 @@ ssid=\"rpicluster-AP\"
 psk=\"rpicluster\"
 }" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
 
+echo "node" > /etc/hostname
+sudo sed -i '6s/.*/127.0.1.1       node/' /etc/hosts
 EOF
