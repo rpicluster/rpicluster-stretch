51eab9f8b9a8cdf639d3ccc4a04ea1d35cad5343
AlecTaggart <taggartalec@gmail.com>
2018-03-28 22:02:02 -0700

    added path to ip config

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -1,4 +1,4 @@
-sudo python config.py &
+sudo python /rpicluster/config/config.py &
 cd /rpicluster/config
 output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 for i in ${output[@]}
