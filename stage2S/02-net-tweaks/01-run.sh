1db29aab6d6465cddd7cd14840f7134fc7b1e176
AlecTaggart <taggartalec@gmail.com>
2018-03-30 09:03:08 -0700

    added access point to build for initial cluster start up

--- stage2S/02-net-tweaks/01-run.sh
+++ stage2S/02-net-tweaks/01-run.sh
@@ -34,4 +34,27 @@ on_chroot << EOF
 sudo echo "sudo python /rpicluster/network-manager/startup.py" >> /home/pi/.profile
 sudo echo "rpicluster" > /etc/hostname
 sudo sed -i '6s/.*/127.0.1.1       rpicluster/' /etc/hosts
+
+sudo apt-get install -y hostapd
+
+sudo echo "interface=wlan0
+driver=nl80211
+ssid=rpicluster-AP
+channel=1
+wmm_enabled=0
+wpa=1
+wpa_passphrase=rpicluster
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+auth_algs=1
+macaddr_acl=0
+logger_stdout=-1
+logger_stdout_level=2
+" | sudo tee /etc/hostapd/hostapd.conf
+
+sudo sed -i '10s/.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+
+sudo sed -i '19s/.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/' /etc/init.d/hostapd
+
 EOF
