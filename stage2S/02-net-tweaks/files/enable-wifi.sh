caf8d0d6434fef41d1434d205af40489a3d40e4a
AlecTaggart <taggartalec@gmail.com>
2018-04-04 17:51:18 -0700

    changed .profile edits from sed to echo | tee

--- stage2S/02-net-tweaks/files/enable-wifi.sh
+++ stage2S/02-net-tweaks/files/enable-wifi.sh
@@ -119,7 +119,8 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 echo "
 Updating startup activities . . .
 "
-sudo sed -i '25s/.*/sudo python /rpicluster/network-manager/link_wifi_adaptor.py/' /home/pi/.profile
+# sudo sed -i '25s/.*/sudo python /rpicluster/network-manager/link_wifi_adaptor.py/' /home/pi/.profile
+sudo echo "sudo python /rpicluster/network-manager/link_wifi_adaptor.py" | sudo tee -a /home/pi/.profile
 
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
