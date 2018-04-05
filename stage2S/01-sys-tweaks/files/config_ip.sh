878a2bea6d6489446ba9151a47b576d258348d63
Derek Dang <dqdang17@gmail.com>
2018-04-04 19:15:05 -0700

    Tweaks to config ip

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -3,8 +3,6 @@ cd /rpicluster/config
 output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 for i in ${output[@]}
 do
-    fab config_ip -u pi -H "$i"
+    fab config_ip -u pi -H "$i" -p "raspberry"
 done
-cd -
-#pkill config.py
-
+sudo reboot -h now
