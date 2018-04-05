19bbef553c65549f51ed424b2e0b4f8762a1c577
AlecTaggart <taggartalec@gmail.com>
2018-04-05 12:34:07 -0700

    Added dns to base

--- stage2S/02-net-tweaks/files/disable-base.sh
+++ stage2S/02-net-tweaks/files/disable-base.sh
@@ -26,3 +26,9 @@ sudo sed -i '10s/.*/#DAEMON_CONF=""/' /etc/default/hostapd
 
 sudo sed -i '19s/.*/#DAEMON_CONF=/' /etc/init.d/hostapd
 
+echo "
+Restoring dnsmasq.conf . . .
+"
+
+sudo rm /etc/dnsmasq.conf
+sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
--- stage2S/02-net-tweaks/files/enable-base.sh
+++ stage2S/02-net-tweaks/files/enable-base.sh
@@ -4,6 +4,12 @@ echo "
 Enabling Base networking scheme . . .
 "
 
+echo "
+Installing host services . . .
+"
+sudo apt-get install -y dnsmasq
+sudo apt-get install -y hostapd
+sudo apt-get install -y rng-tools
 
 echo "
 Updating dhcpcd.conf . . .
@@ -11,7 +17,6 @@ Updating dhcpcd.conf . . .
 sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
 
 sudo echo "interface wlan0
-metric 150
 static ip_address=192.168.1.254/24
 #static routers=192.168.1.1
 static domain_name_servers=8.8.8.8" | sudo tee -a /etc/dhcpcd.conf
@@ -48,3 +53,20 @@ Linking new hostapd.conf . . .
 sudo sed -i '10s/.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
 
 sudo sed -i '19s/.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/' /etc/init.d/hostapd
+
+echo "
+Generating new dnsmasq.conf . . .
+"
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+
+sudo echo "no-resolv
+interface=wlan0
+listen-address=192.168.1.254
+server=8.8.8.8 # Use Google DNS
+domain-needed # Don't forward short names
+bogus-priv # Drop the non-routed address spaces.
+dhcp-range=192.168.1.100,192.168.1.150,12h # IP range and lease time
+#log each DNS query as it passes through
+log-queries
+dhcp-authoritative
+" | sudo tee /etc/dnsmasq.conf
