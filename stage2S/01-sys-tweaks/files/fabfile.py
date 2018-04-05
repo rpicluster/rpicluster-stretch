86e7d35aab5a83554461ea9097efd9e4c5f15e30
Derek Dang <dqdang17@gmail.com>
2018-04-04 19:15:05 -0700

    Tweaks to config ip

--- stage2S/01-sys-tweaks/files/fabfile.py
+++ stage2S/01-sys-tweaks/files/fabfile.py
@@ -3,7 +3,6 @@ from fabric.api import *
 def config_ip():
     with cd('/rpicluster/config'):
         run('sudo python calibrate_touchless.py')
-    run('sudo reboot -h now')
 
 def send_SSH_keys(ssh_key):
     run('mkdir ~/.ssh/')
