98108a02e7b2c0cddb4d84d8142fccc4b32c61ad
Derek Dang <dqdang17@gmail.com>
2018-04-10 13:13:59 -0700

    initial commit of setting hostname

--- stage2S/01-sys-tweaks/files/fabfile.py
+++ stage2S/01-sys-tweaks/files/fabfile.py
@@ -1,9 +1,14 @@
 from fabric.api import *
 
-def config_ip():
-    with cd('/rpicluster/config'):
-        run('sudo python calibrate_touchless.py')
-    run('sudo reboot -h now')
+def config_ip(num):
+    with settings(warn_only = True):
+        rv = False
+        with cd('/rpicluster/config'):
+            rv = run('sudo python calibrate_touchless.py')
+        if(rv == True):
+            with cd('/etc/hostname'):
+                run('echo node{} > hostname'.format(num))    
+            run('sudo reboot -h now')
 
 def send_SSH_keys(ssh_key):
     run('mkdir ~/.ssh/')
--- stage2S/01-sys-tweaks/files/send_ssh_keys.sh
+++ stage2S/01-sys-tweaks/files/send_ssh_keys.sh
@@ -2,5 +2,5 @@ key=`ssh-keygen -t rsa -C pi@192.168.1.254`
 output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 for i in ${output[@]}
 do
-    fab send_SSH_keys "$key" -u pi -H "$i"
+    fab send_SSH_keys "$key" -u pi -H "$i" -p "raspberry"
 done
