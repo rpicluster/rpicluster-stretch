9eea3a8ae25430150ce992ddcbbdfe9263b4d83a
AlecTaggart <taggartalec@gmail.com>
2018-04-11 15:26:27 -0700

    lol

--- stage2S/02-net-tweaks/files/startup.py
+++ stage2S/02-net-tweaks/files/startup.py
@@ -9,7 +9,7 @@ if(network == "0"):
     print("\n- - - Congratulations on starting up your rpicluster ! - - -\n")
     os.system("python /rpicluster/network-manager/network-manager.py")
 configured = f.read(1)
-else if(int(network) > 0 and configured == "0"):
+if(int(network) > 0 and configured == "0"):
 	print("\nWARNING: Looks like your nodes are not configured !")
 	print("Run rpicluster-configure to configure the cluster !\n")
 
