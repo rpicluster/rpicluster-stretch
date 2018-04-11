8bcaa4b69f955fb379d39b4156a37fc147d8df6c
Derek Dang <dqdang17@gmail.com>
2018-04-11 10:32:02 -0700

    Forgot password in fabfile

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -4,13 +4,13 @@ output=`python -c 'from list_leases import *; print " ".join(getMachines())'`
 counter=0
 for i in ${output[@]}
 do
-    rv=$(fab pingall -u pi -H "$i" --abort-on-prompts --hide warnings,stdout,aborts,status,running)
+    rv=$(fab pingall -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running)
     echo rv \= $rv
     if [ $rv -eq 0 ];
     then
         counter=$((counter+1))
     fi
     echo $counter
-    fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide stdout,warnings
+    fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running
 done
 sudo reboot -h now
