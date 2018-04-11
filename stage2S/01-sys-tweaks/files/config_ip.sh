e449ec91ac76863fa6fbe3cb44208a25e6e06dd1
Derek Dang <dqdang17@gmail.com>
2018-04-10 21:43:45 -0700

    fabric change to remove warnings and add hostname

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -5,6 +5,6 @@ counter=0
 for i in ${output[@]}
 do
     counter=$((counter+1))
-    fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts
+    fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings
 done
 sudo reboot -h now
