5e0908051fdfe2b63732b4d97c9c4ce98a3d0b97
Derek Dang <dqdang17@gmail.com>
2018-04-11 13:53:18 -0700

    fix path

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -1,15 +1,18 @@
 sudo python /rpicluster/config/config.py &
-# cd /rpicluster/config
+cd /rpicluster/config
 output=`python -c 'from functions import *; print " ".join(get_machines())'`
 counter=0
-
+sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
 for i in ${output[@]}
 do
+    echo "Attempting to configure machine at IP: $i"
     rv=$(fab pingall -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running)
     if [ $rv -eq 0 ];
     then
         counter=$((counter+1))
+        fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,running
+        echo "Node configured"
     fi
-    fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,running
 done
 sudo reboot -h now
+
