8219634d259433bfffc33d4a985afd706d11da97
Derek Dang <dqdang17@gmail.com>
2018-04-11 15:14:29 -0700

    add hide for ssh keys

--- stage2S/01-sys-tweaks/files/send_ssh_keys.sh
+++ stage2S/01-sys-tweaks/files/send_ssh_keys.sh
@@ -1,7 +1,7 @@
 key=`ssh-keygen -t rsa -C pi@192.168.1.254`
 cd /rpicluster/config
-output=`python -c 'from functions import *; print " ".join(get_machines())'`
+output=`python -c 'from functions import *; print " ".join(get_nodes())'`
 for i in ${output[@]}
 do
-    fab send_SSH_keys "$key" -u pi -H "$i" -p "raspberry"
+    fab send_SSH_keys:"$key" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,running
 done
