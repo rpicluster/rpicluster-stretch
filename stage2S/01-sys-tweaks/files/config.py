e2311c2bfe4ca8ba2fdabe6f9e0a40d98bfa3ff1
Derek Dang <dqdang17@gmail.com>
2018-04-11 13:53:18 -0700

    fix path

--- stage2S/01-sys-tweaks/files/config.py
+++ stage2S/01-sys-tweaks/files/config.py
@@ -6,12 +6,11 @@ class IpSender(object):
     def sendIP(self, mac):
         self.ip+=1
         desiredIp = "192.168.1." + str(self.ip)
-        os.system("sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf")
         with open("/etc/dnsmasq.conf", "a") as configFile:
             configFile.write("dhcp-host=" + mac + "," + desiredIp + "\n")
         with open("/rpicluster/config/nodes", "wb") as nodeFile:
             nodeFile.write(desiredIp + "," + mac + "," + "node" + str(self.ip) + "\n")
-        print(desiredIp + ", " + mac + ", " + "node" + str(self.ip))
+        print("Configured IP: " + desiredIp + " on node" + str(self.ip) + " - " + mac)
         return str(desiredIp)
 
 
