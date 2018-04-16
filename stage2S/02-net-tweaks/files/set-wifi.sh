4a9f1291631bab8177a4d48bbfd96e2709284d5d
AlecTaggart <taggartalec@gmail.com>
2018-04-16 08:55:37 -0700

    updated set-wifi

--- stage2S/02-net-tweaks/files/set-wifi.sh
+++ stage2S/02-net-tweaks/files/set-wifi.sh
@@ -1,11 +1,20 @@
-echo "- - - Set Up Wifi Interface - - -"
-echo " "
-read -p "Enter network ssid: " ssid
-read -sp "Enter network psk: " psk
 
-echo
 
-sudo echo "network={
-    ssid=\"$ssid\"
-    psk=\"$psk\"
-}" >> /etc/wpa_supplicant/$1
+read -p "Would you like to connect your cluster to the internet? (y/n) " answer
+
+if [[ $answer == 'y' ]]; then
+	
+	echo "- - - Set Up Wifi Interface - - -"
+	echo " "
+	read -p "Enter network ssid: " ssid
+	read -sp "Enter network psk: " psk
+
+	echo
+
+	sudo echo "network={
+	    ssid=\"$ssid\"
+	    psk=\"$psk\"
+	}" >> /etc/wpa_supplicant/$1
+
+fi
+
