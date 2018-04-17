a089f64ef42e57998208a0cf3047e9a216bda241
AlecTaggart <taggartalec@gmail.com>
2018-04-17 13:37:14 -0700

    updated spacing

--- stage2S/02-net-tweaks/files/set-wifi.sh
+++ stage2S/02-net-tweaks/files/set-wifi.sh
@@ -1,18 +1,18 @@
 
 
 read -p "Would you like to connect your cluster to the internet? (y/n) " answer
-
+echo " "
 
 if [[ $answer == 'y' ]]; then
 	
 	echo "- - - Set Up Wifi Interface - - -"
 	echo " "
 	read -p "Make sure to plug in the wifi adaptor! Press enter to continue." none
-	
+	echo " "
 	read -p "Enter network ssid: " ssid
 	read -sp "Enter network psk: " psk
 
-	echo
+	echo " "
 
 	sudo echo "network={
 	    ssid=\"$ssid\"
