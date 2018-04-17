fd425b074a5e3fe5866f9e0b70a1f9dcff8d859a
AlecTaggart <taggartalec@gmail.com>
2018-04-17 16:54:27 -0700

    Moved all en/disable scripts to new format

--- stage2S/02-net-tweaks/files/disable-base.sh
+++ stage2S/02-net-tweaks/files/disable-base.sh
@@ -1,34 +1,60 @@
-#!/bin/bash -e
+#!/bin/bash
 
 
 echo "
 Disabling Base networking scheme . . .
 "
 
-echo "
-Restoring dhcpcd.conf . . .
-"
-sudo rm /etc/dhcpcd.conf
-sudo mv /etc/dhcpcd.conf.orig /etc/dhcpcd.conf
+count=1
+total=4
+start=`date +%s`
+
+while [ $count -le $total ]; do
+
+    if [ $count -eq 1 ]
+    	then
+
+    	task="Restoring dhcpcd.conf"
+    	sudo rm /etc/dhcpcd.conf
+		sudo mv /etc/dhcpcd.conf.orig /etc/dhcpcd.conf
+
+    
+	elif [ $count -eq 2 ]
+		then
+
+		task="Removing hostapd.conf"
+		sudo rm /etc/hostapd/hostapd.conf
+	
+	elif [ $count -eq 3 ] 
+		then
 
 
-echo "
-Removing hostapd.conf . . .
-"
-sudo rm /etc/hostapd/hostapd.conf
+		task="Unlinking hostapd.conf"
+		sudo sed -i '10s/.*/#DAEMON_CONF=""/' /etc/default/hostapd
+		sudo sed -i '19s/.*/#DAEMON_CONF=/' /etc/init.d/hostapd
+
+	elif [ $count -eq 4 ] 
+		then
+
+		task="Restoring dnsmasq.conf"
+		sudo rm /etc/dnsmasq.conf
+		sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
 
 
-echo "
-Unlinking hostapd.conf . . .
-"
+	
+	else
+		task="Finished"
 
-sudo sed -i '10s/.*/#DAEMON_CONF=""/' /etc/default/hostapd
+	fi
+	cur=`date +%s`
+	
+    runtime=$(( $cur-$start ))
+    estremain=$(( ($runtime * $total / $count)-$runtime ))
+    printf "\r%d.%d%% complete ($count of $total tasks) - est %d:%0.2d remaining - $task\e[K" $(( $count*100/$total )) $(( ($count*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
+    if [ $count -lt 6 ]
+		then
+        count=$(( $count + 1 ))
+    fi
+done
+printf "\r%d.%d%% complete (4 of 4) - est %d:%0.2d remaining - Finished\e[K" $(( 4*100/$total )) $(( (4*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
 
-sudo sed -i '19s/.*/#DAEMON_CONF=/' /etc/init.d/hostapd
-
-echo "
-Restoring dnsmasq.conf . . .
-"
-
-sudo rm /etc/dnsmasq.conf
-sudo mv /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
--- stage2S/02-net-tweaks/files/disable-wifi.sh
+++ stage2S/02-net-tweaks/files/disable-wifi.sh
@@ -1,48 +1,72 @@
-#!/bin/bash -e
+#!/bin/bash
 
 
 echo "
 Disabling Wifi-Wifi networking scheme . . .
 "
-echo "
-removing wlan1 wpa_supplicant . . .
-"
-sudo rm /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+
+count=1
+total=6
+start=`date +%s`
+
+while [ $count -le $total ]; do
+
+    if [ $count -eq 1 ]
+    	then
+
+    	task="Removing wpa_supplicant"
+    	sudo rm /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 
 
-echo "
-Restoring dhcpcd.conf . . .
-"
-sudo rm /etc/dhcpcd.conf
-sudo mv /etc/dhcpcd.conf.orig /etc/dhcpcd.conf
+	elif [ $count -eq 2 ]
+		then
+
+		task="Restoring dhcpcd.conf"
+		sudo rm /etc/dhcpcd.conf
+		sudo mv /etc/dhcpcd.conf.orig /etc/dhcpcd.conf
+
+		
+	elif [ $count -eq 3 ] 
+		then
+
+		task="Removing hostapd.conf"
+		sudo rm /etc/hostapd/hostapd.conf
 
 
-echo "
-Removing hostapd.conf . . .
-"
-sudo rm /etc/hostapd/hostapd.conf
+	elif [ $count -eq 4 ] 
+		then
 
+		task="Unlinking hostapd.conf"
+		sudo sed -i '10s/.*/#DAEMON_CONF=""/' /etc/default/hostapd
+		sudo sed -i '19s/.*/#DAEMON_CONF=/' /etc/init.d/hostapd
 
-echo "
-Unlinking hostapd.conf . . .
-"
+		
+	elif [ $count -eq 5 ] 
+		then
 
-sudo sed -i '10s/.*/#DAEMON_CONF=""/' /etc/default/hostapd
+		task="Restoring dnsmasq.conf"
+		sudo rm /etc/dnsmasq.conf
+		sudo touch /etc/dnsmasq.conf
+		
+	elif [ $count -eq 6 ] 
+		then
 
-sudo sed -i '19s/.*/#DAEMON_CONF=/' /etc/init.d/hostapd
+		task="Restoring rc.local"
+		sed -i '20d' /etc/rc.local
 
+	else
+		task="Finished"
 
-echo "
-Restoring dnsmasq.conf . . .
-"
-
-sudo rm /etc/dnsmasq.conf
-sudo touch /etc/dnsmasq.conf
-
-echo "
-Restoring rc.local / .profile . . .
-"
-
-sed -i '20d' /etc/rc.local
-# sudo sed -i '25d' /home/pi/.profile
+	fi
+	cur=`date +%s`
+	
+    runtime=$(( $cur-$start ))
+    estremain=$(( ($runtime * $total / $count)-$runtime ))
+    printf "\r%d.%d%% complete ($count of $total tasks) - est %d:%0.2d remaining - $task\e[K" $(( $count*100/$total )) $(( ($count*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
+    if [ $count -lt 8 ]
+		then
+        count=$(( $count + 1 ))
+    fi
+done
+printf "\r%d.%d%% complete (6 of 6) - est %d:%0.2d remaining - Finished\e[K" $(( 6*100/$total )) $(( (6*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
 
