1c32b54d9d24625ad7299ca7d9a9a5462ce7c95d
AlecTaggart <taggartalec@gmail.com>
2018-04-17 16:04:14 -0700

    fixed syntax finally

--- stage2S/02-net-tweaks/files/enable-wifi.sh
+++ stage2S/02-net-tweaks/files/enable-wifi.sh
@@ -5,13 +5,13 @@ echo "
 Enabling Wifi-Wifi networking scheme . . .
 "
 
-count=0
+count=1
 total=9
 start=`date +%s`
 
 while [ $count -le $total ]; do
 
-    if [ $count -eq 0 ]
+    if [ $count -eq 1 ]
     	then
 
     	task="Configuring Wifi Adaptor"
@@ -20,14 +20,14 @@ while [ $count -le $total ]; do
 		sudo python /rpicluster/network-manager/link_wifi_adaptor.py wlan1
 		echo " "
 
-	elif [ $count -eq 1 ]
+	elif [ $count -eq 2 ]
 		then
 
 		task="Installing host services"
 		sudo apt-get install -y dnsmasq &> /dev/null
 		sudo apt-get install -y hostapd &> /dev/null
 		sudo apt-get install -y rng-tools &> /dev/null
-	elif [ $count -eq 2 ] 
+	elif [ $count -eq 3 ] 
 		then
 
 
@@ -42,7 +42,7 @@ static domain_name_servers=8.8.8.8 #192.168.1.1
 
 interface wlan1
 metric 100" >> /etc/dhcpcd.conf
-	elif [ $count -eq 3 ] 
+	elif [ $count -eq 4 ] 
 		then
 
 		task="Generating new hostapd.conf"
@@ -60,14 +60,14 @@ auth_algs=1
 macaddr_acl=0
 logger_stdout=-1
 logger_stdout_level=2" > /etc/hostapd/hostapd.conf
-	elif [ $count -eq 4 ] 
+	elif [ $count -eq 5 ] 
 		then
 
 		task="Linking new hostapd.conf"
 		sudo sed -i '10s/.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
 		sudo sed -i '19s/.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/' /etc/init.d/hostapd
 	
-	elif [ $count -eq 5 ] 
+	elif [ $count -eq 6 ] 
 		then
 
 		task="Generating new dnsmasq.conf"
@@ -84,7 +84,7 @@ log-queries
 dhcp-authoritative" > /etc/dnsmasq.conf
 
 		sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-	elif [ $count -eq 6 ] 
+	elif [ $count -eq 7 ] 
 		then
 		
 		task="Generating new iptable Rules"
@@ -93,13 +93,13 @@ dhcp-authoritative" > /etc/dnsmasq.conf
 		sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE 
 		sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 		sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
-	elif [ $count -eq 7 ]
+	elif [ $count -eq 8 ]
 		then
 
 		task="Allowing ip_forward"
 		sudo sed -i '28 s/#//' /etc/sysctl.conf
 		sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-	elif [ $count -eq 8 ]
+	elif [ $count -eq 9 ]
 		then
 
 		task="Updating startup activities"
@@ -107,14 +107,18 @@ dhcp-authoritative" > /etc/dnsmasq.conf
 		sudo sed -i '20i\iptables-restore < \/etc\/iptables.ipv4.nat\' /etc/rc.local
 	else
 		task="Finished"
+
 	fi
 	cur=`date +%s`
-	if [ $count -ne 9 ]
-		then
-        count=$(( $count + 1 ))
-    fi
-    echo "$count"
+	
+    echo ""
     runtime=$(( $cur-$start ))
     estremain=$(( ($runtime * $total / $count)-$runtime ))
     printf "\r%d.%d%% complete ($count of $total tasks) - est %d:%0.2d remaining - $task\e[K" $(( $count*100/$total )) $(( ($count*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
+    if [ $count -lt 11 ]
+		then
+        count=$(( $count + 1 ))
+    fi
 done
+printf "\r%d.%d%% complete (9 of 9) - est %d:%0.2d remaining - Finished\e[K" $(( 9*100/$total )) $(( (9*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
+
