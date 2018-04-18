d7c269d9a85e4304657e037dfef20071c0c4a456
Derek Dang <dqdang17@gmail.com>
2018-04-18 15:14:32 -0700

    fix unary bug

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -10,14 +10,14 @@ for i in ${output[@]}
 do
     echo "Attempting to configure machine at IP: $i"
     rv=$(fab pingall -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running)
-    printf($rv)
-    if [ $rv = "0" ];
+
+    if [ $rv ];
     then
         counter=$((counter+1))
         fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running
         echo -e "Node configured.\n"
     else
-    	echo -e "Skip configuration of non-node device.\n"
+        echo -e "Skip configuration of non-node device.\n"
     fi
 
 done
