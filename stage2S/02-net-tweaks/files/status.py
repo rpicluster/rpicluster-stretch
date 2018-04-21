a14fea64b02dcc799cec436c718925b6f85a1486
AlecTaggart <taggartalec@gmail.com>
2018-04-21 00:19:44 -0700

    fixed status tool and updated TODO

--- stage2S/02-net-tweaks/files/status.py
+++ stage2S/02-net-tweaks/files/status.py
@@ -19,27 +19,34 @@ connection_name = ""
 if(network != 0):
 
     if(network == 1):
-        internet_ip = get_ip(ip_output, "wlan1")
+        internet_ip = str(get_ip(ip_output, "wlan1"))
         internet_name = "wlan1"
-        access_point = get_ip(ip_output, "wlan0")
+        access_point = str(get_ip(ip_output, "wlan0"))
         connection_name = "Access Point"
 
     elif(network == 2):
-        internet_ip = get_ip(ip_output, "wlan0")
+        internet_ip = str(get_ip(ip_output, "wlan0"))
         internet_name = "wlan0"
-        access_point = get_ip(ip_output, "eth0")
+        access_point = str(get_ip(ip_output, "eth0"))
         connection_name = "Switch"
 
     # else if(network == 3):
-
     print("\nCurrent network configuration: " + network_type(network))
-    print("\nInternet on " + internet_name + "--> " + str(internet_ip))
-    print("                            |")
-    print("                            |")
-    print("                            --> "+ connection_name + "--> " + str(access_point))
-    for x in range(len(machines)):
-        print("                                                       |")
-        print("                                                       --> " + machines[x][0] + " - " + machines[x][1])
+
+    if(internet_ip != "None"):
+        print("                            |")
+        print("                            |")
+        print("                            --> "+ connection_name + "--> " + access_point)
+        for x in range(len(machines)):
+            print("                                                       |")
+            print("                                                       --> " + machines[x][0] + " - " + machines[x][1])
+    else:
+        print("                       |")
+        print("                       |")
+        print("                       --> "+ connection_name + "--> " + access_point)
+        for x in range(len(machines)):
+            print("                                                  |")
+            print("                                                  --> " + machines[x][0] + " - " + machines[x][1])
 
 else:
     print("\nNo rpicluster Network configured ! ! !\n")
