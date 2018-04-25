3f335e9b7c04f3114c967f812ea8cade94ac9107
Derek Dang <dqdang17@gmail.com>
2018-04-24 19:13:10 -0700

    remove warnings

--- stage2S/02-net-tweaks/01-run.sh
+++ stage2S/02-net-tweaks/01-run.sh
@@ -49,6 +49,8 @@ sudo sed -i '35s/.*/StrictHostKeyChecking no/' /etc/ssh/ssh_config
 
 sudo sed -i '36s/.*/UserKnownHostsFile \/dev\/null/' /etc/ssh/ssh_config
 
+sudo sed -i '37s/.*/LogLevel QUIET' /etc/ssh/ssh_config
+
 sudo cp /etc/hosts /etc/hosts.orig
 
 
