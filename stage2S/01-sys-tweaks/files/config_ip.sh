cad5ff6de4b768d101d810a52451270997f4e56f
Derek Dang <dqdang17@gmail.com>
2018-04-26 14:12:15 -0700

    fixing eq unary bug

--- stage2S/01-sys-tweaks/files/config_ip.sh
+++ stage2S/01-sys-tweaks/files/config_ip.sh
@@ -7,9 +7,6 @@ sudo cp /etc/hosts.orig /etc/hosts
 sudo rm nodes
 sudo touch nodes
 sudo chmod 777 nodes
-zero=0
-zero_string="0"
-amount=0
 sudo echo "#MPI CLUSTER SETUP" >> /etc/hosts
 sudo echo "192.168.1.254    rpicluster" >> /etc/hosts
 sudo echo "rpicluster slots=1  max-slots=1" > /home/pi/nfs/mpi/mpiHosts
@@ -18,17 +15,22 @@ do
     echo "Attempting to configure machine at IP: $i"
     rv=$(fab pingall -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running,stderr)
 
-    if [ $rv -eq $zero ] || [ $rv == $zero_string ];
+    if [ $rv ];
+    then
+        rv=0
+    else
+        rv=1
+
+    if [ $rv -eq 0 ];
     then
         counter=$((counter+1))
         fab config_ip:"$counter" -u pi -H "$i" -p "raspberry" --abort-on-prompts --hide warnings,stdout,aborts,status,running,stderr
         echo -e "Node configured.\n"
-        amount=$((amount + 1))
     else
         echo -e "Skip configuration of non-node device.\n"
     fi
 
 done
-echo "$amount nodes configured."
+echo "$counter nodes configured."
 sed -i 's/./1/2' /rpicluster/network-manager/configured
 
