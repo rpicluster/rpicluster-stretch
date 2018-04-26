4044e76f59733e2fd45afe46400ee4a38ad6aaa9
Derek Dang <dqdang17@gmail.com>
2018-04-26 15:36:08 -0700

    added extra locale fixes

--- stage2C/02-net-tweaks/01-run.sh
+++ stage2C/02-net-tweaks/01-run.sh
@@ -15,6 +15,12 @@ echo "node" > /etc/hostname
 sudo sed -i '6s/.*/127.0.1.1       node/' /etc/hosts
 sudo cp /etc/hosts /etc/hosts.orig
 
+sudo sed -i '157 s/#//' /etc/locale.gen
+
+sudo locale-gen en_US.UTF-8
+
+sudo update-locale en_US.UTF-8
+
 sudo echo "LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 LANGUAGE=en_US.UTF-8" >> /etc/default/locale
