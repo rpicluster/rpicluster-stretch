7d71ec0beac52a0d388eb1cc4be1f9270cddef0a
Derek Dang <dqdang17@gmail.com>
2018-04-26 13:53:16 -0700

    fix default locale

--- stage2C/02-net-tweaks/01-run.sh
+++ stage2C/02-net-tweaks/01-run.sh
@@ -14,4 +14,9 @@ on_chroot << EOF
 echo "node" > /etc/hostname
 sudo sed -i '6s/.*/127.0.1.1       node/' /etc/hosts
 sudo cp /etc/hosts /etc/hosts.orig
+
+sudo echo "LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LANGUAGE=en_US.UTF-8" >> /etc/default/locale
+
 EOF
