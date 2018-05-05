9722c9e6634ae6e04e954fcd6747545210de7f13
AlecTaggart <taggartalec@gmail.com>
2018-05-04 17:47:02 -0700

    capitalize S

--- dev_update.py
+++ dev_update.py
@@ -1,7 +1,7 @@
 import os
 
 def update_files():
-    with open("stage2s/01-sys-tweaks/01-run.sh", "r") as sysrun:
+    with open("stage2S/01-sys-tweaks/01-run.sh", "r") as sysrun:
         sysrun.readline()
         line = sysrun.readline()
         while(not line):
@@ -12,7 +12,7 @@ def update_files():
                 break
             line = sysrun.readline()
 
-    with open("stage2s/02-net-tweaks/01-run.sh", "r") as netrun:
+    with open("stage2S/02-net-tweaks/01-run.sh", "r") as netrun:
         netrun.readline()
         line = netrun.readline()
         while(not line):
