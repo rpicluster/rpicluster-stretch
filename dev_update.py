64e826b701fc893f86168ca530f18c3dc82b34bf
AlecTaggart <taggartalec@gmail.com>
2018-05-04 17:56:02 -0700

    update dev tool

--- dev_update.py
+++ dev_update.py
@@ -2,25 +2,19 @@ import os
 
 def update_files():
     with open("stage2S/01-sys-tweaks/01-run.sh", "r") as sysrun:
-        sysrun.readline()
-        line = sysrun.readline()
-        while(not line):
+        for line in sysrun:
             print(line)
             if(line.startswith("install")):
                 os.system("sudo" + line)
             else:
                 break
-            line = sysrun.readline()
 
     with open("stage2S/02-net-tweaks/01-run.sh", "r") as netrun:
-        netrun.readline()
-        line = netrun.readline()
-        while(not line):
+        for line in netrun:
             print(line)
             if(line.startswith("install")):
                 os.system("sudo" + line)
             else:
                 break
-            line = netrun.readline()
 
 update_files()
