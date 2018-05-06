8eb7ae74f633683c2f00071a7a8416f726cb897b
AlecTaggart <taggartalec@gmail.com>
2018-05-06 15:39:03 -0700

    update xgboost install

--- stage2S/01-sys-tweaks/files/xgboost-install.sh
+++ stage2S/01-sys-tweaks/files/xgboost-install.sh
@@ -1,5 +1,7 @@
 #XGBOOST SETUP
-sudo git clone --recursive https://github.com/dmlc/xgboost /home/pi/nfs/packages
+if [ ! -d "/home/pi/nfs/packages/xgboost" ]; then
+    sudo git clone --recursive https://github.com/dmlc/xgboost /home/pi/nfs/packages/xgboost
+fi
 cd /home/pi/nfs/packages/xgboost
 sudo sed -i '22s/.*/export CFLAGS = -O3 $(WARNFLAGS)/' Makefile
 make -j4
