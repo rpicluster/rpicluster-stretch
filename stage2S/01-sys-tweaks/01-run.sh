6b71a45632c23f56fe671d02a4ab1b36be982ca7
AlecTaggart <taggartalec@gmail.com>
2018-05-05 20:11:33 -0700

    please

--- stage2S/01-sys-tweaks/01-run.sh
+++ stage2S/01-sys-tweaks/01-run.sh
@@ -70,6 +70,7 @@ install -m 777 files/stamp                              "${ROOTFS_DIR}/boot"
 
 install -m 777 files/exports                            "${ROOTFS_DIR}/etc/"
 
+sudo cp files/xgboost "${ROOTFS_DIR}/home/pi/"
 
 
 
@@ -132,7 +133,6 @@ sudo apt-get -y install python3-scipy
 sudo apt-get -y install python3-sklearn
 sudo apt-get -y install python-setuptools
 sudo apt-get -y install libblas-dev liblapack-dev libatlas-base-dev gfortran
-sudo cp files/xgboost /home/pi/
 cd /home/pi/xgboost
 sudo sed -i '22s/.*/export CFLAGS = -O3 $(WARNFLAGS)/' /etc/default/hostapd
 make -j4
