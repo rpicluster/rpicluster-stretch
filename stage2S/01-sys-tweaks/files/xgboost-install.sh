75b9d664242b34a1a25d5f42687b7f9df435b5a7
AlecTaggart <taggartalec@gmail.com>
2018-05-07 12:43:46 -0700

    finally done

--- stage2S/01-sys-tweaks/files/xgboost-install.sh
+++ stage2S/01-sys-tweaks/files/xgboost-install.sh
@@ -1,8 +1,14 @@
 #XGBOOST SETUP
 if [ ! -d "/home/pi/nfs/packages/xgboost" ]; then
     sudo git clone --recursive https://github.com/dmlc/xgboost /home/pi/nfs/packages/xgboost
+    sudo sed -i '22s/.*/export CFLAGS = -O3 $(WARNFLAGS)/' /home/pi/nfs/packages/xgboost/rabit/Makefile
 fi
+#sudo apt-get -y install python3-numpy
+#sudo apt-get -y install python3-scipy
+#sudo apt-get -y install python3-sklearn
+#sudo apt-get -y install python-setuptools
+#sudo apt-get -y install libblas-dev liblapack-dev libatlas-base-dev gfortran
 cd /home/pi/nfs/packages/xgboost
-sudo sed -i '22s/.*/export CFLAGS = -O3 $(WARNFLAGS)/' Makefile
-make -j4
+sudo make -j4
 cd /home/pi/nfs/packages/xgboost/python-package; sudo python3 setup.py install
+
